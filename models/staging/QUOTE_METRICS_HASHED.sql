16e08e57d3be411e3238b057ae7db0a1cbe86dab
Roberto Zagni <rz70rz70@gmail.com>
2020-06-07 22:48:35 +0300

    Adding stage for securities listing and metrics from existing quotes.

---
+++ models/staging/QUOTE_METRICS_HASHED.sql
@@ -0,0 +1,25 @@
+WITH quotes AS (
+    SELECT * FROM {{ source('spytm', 'daily_quote') }}
+)
+SELECT
+    symbol,
+    min(date) as min_date,
+    max(date) as max_date,
+    count(date) as num_quotes,
+    min(open) as min_open,
+    max(open) as max_open,
+    min(close) as min_close,
+    max(close) as max_close,
+    min(low) as min_low,
+    max(high) as max_high,
+    avg(open) as avg_open,
+    avg(close) as avg_close,
+    min(volume) as min_volume,
+    max(volume) as max_volume,
+    avg(volume) as avg_volume,
+    'spytm.daily_quote' as RECORD_SOURCE,
+    (max(date)||' 16:30:00.000')::timestamp as LOAD_TS,
+    min(date) as EFFECTIVE_FROM,
+    {{ dbt_utils.surrogate_key(['symbol']) }} as SECURITY_PK
+FROM quotes
+GROUP BY symbol
